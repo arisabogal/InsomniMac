304d6bd6930b21915ad2ffe00d69dea1efd524eb
arisabogal <sabogalari@gmail.com>
2026-03-30 13:56:40 -0700

    Initial Commit

---
+++ InsomniMac/InsomniMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsomniMacApp.swift
+//  InsomniMac
+//
+//  Created by Ari Sabogal on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct InsomniMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InsomniMacTests/InsomniMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  InsomniMacTests.swift
+//  InsomniMacTests
+//
+//  Created by Ari Sabogal on 3/30/26.
+//
+
+import Testing
+@testable import InsomniMac
+
+struct InsomniMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
